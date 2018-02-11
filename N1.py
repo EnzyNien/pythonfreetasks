9b63a799ce0ede086d4bee5a29db3a9af80d5407
EnzyNien <int_21h@mail.ru>
2018-02-11 19:12:07 +0300

    Реализовать собственный key-value storage
    Реализовать собственный key-value storage. Вашей задачей будет написать скрипт, который принимает в качестве аргументов ключи и значения и выводит информацию из хранилища (в нашем случае — из файла). 
    
    Запись значения по ключу 
    > storage.py —key key_name —val value 
    Получение значения по ключу 
    > storage.py —key key_name 
    
    Ответом в данном случае будет вывод с помощью print соответствующего значения 
    > value 
    или 
    > value_1, value_2 
    
    если значений по этому ключу было записано несколько. Метрики сохраняйте в порядке их добавления. Обратите внимание на пробел после запятой. 
    
    Если значений по ключу не было найдено, выводите пустую строку или None. 
    
    Для работы с аргументами командной строки используйте модуль argparse. Вашей задачей будет считать аргументы, переданные вашей программе, и записать соответствующую пару ключ-значение в файл хранилища или вывести значения, если был передан только ключ. Хранить данные вы можете в формате JSON с помощью стандартного модуля json. Проверьте добавление нескольких ключей и разных значений. 
    
    Файл следует создавать с помощью модуля tempfile.
    
    import os
    import tempfile
    
    storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
    with open(storage_path, 'w') as f:
    ...

---
+++ N1.py
@@ -0,0 +1,54 @@
+import os
+import tempfile
+import json
+import argparse
+
+
+def Actions(key=None,value=None,filename='storage.data'):
+
+	storage_path = os.path.join(tempfile.gettempdir(), filename)
+
+	if os.path.exists(storage_path):
+		with open(storage_path, "r+") as f:
+			readJson = json.load(f)
+	else:
+		with open(storage_path, "w") as f:
+			readJson = {"keys":[],"values":{}}
+			json.dump(readJson,f)
+
+	if value is None:
+		values = readJson['values'].get(key,[])
+		print("key {}, values: {}". format(key,", ".join([str(i) for i in values])))
+		return
+
+	if key in readJson['keys']:
+		valuesList = readJson['values'][key]
+		valuesList.append(value)
+		readJson['values'][key] = valuesList
+	else:
+		keysList = readJson['keys']
+		keysList.append(key)
+		readJson['keys'] = keysList
+		readJson['values'].update({key:[value]})
+	
+	with open(storage_path, "w") as f:
+		json.dump(readJson,f)
+
+
+def main():
+	
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-key","-k", type = str, help ="parameters - key. type - str")
+	parser.add_argument("-value","-v", type = str, help ="parameters - value. type - str")
+	
+	args= parser.parse_args()
+	if args.key is None:
+		print("Error. -key is not defined")
+		return
+	_key = str(args.key)
+	_value = args.value
+	Actions(_key,_value)
+
+
+if __name__ == '__main__':
+	main()

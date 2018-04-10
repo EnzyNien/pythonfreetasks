2a7128892138f6be4506ef5e6dad2964c4b74e58
EnzyNien <int_21h@mail.ru>
2018-04-10 08:30:40 +0300

    Update N36.py

--- N36.py
+++ N36.py
@@ -173,9 +173,9 @@ for row in names:
 		tank_description = tank_description[str(tank_id)]
 		tank_name = tank_description['name']
 		print(f'{idx}) tank_name: {tank_name}, tank_id: {tank_id}\nwins/battles: {wins} / {battles}')
-
+		
 		if tank_id in write_tanks:
-			print(f'previously saved image from: {image_url}')
+			print(f'previously saved image from: {image_url}/n')
 		else:
 			image_url = tank_description['images']['big_icon']
 			image_name = image_url.split('/')[-1]
@@ -184,7 +184,7 @@ for row in names:
 				with open(os.path.join(Wg.dir_name,image_name), 'wb') as f:
 					f.write(response.content)
 					write_tanks.append(tank_id)
-					
+					print(f'download image from: {image_url}/n')
 			else:
-				print(f'error download image from: {image_url}')
+				print(f'error download image from: {image_url}/n')
 		
